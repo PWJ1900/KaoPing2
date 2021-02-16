a04a4a5b0742cf1228721c523cbb25c4fa76c841
王晓伟 <930217906@qq.com>
2021-02-16 13:36:40 +0800

    w

--- src/components/dialog/zbtxDialog.vue
+++ src/components/dialog/zbtxDialog.vue
@@ -13,22 +13,25 @@
             <table>
               <!-- 指标体系 -->
               <div>
-                <table>
+                <table border="2" width="500px">
                   <tr>
                     <td rowspan="3">指标体系</td>
                     <td>
-                      体系名称定义：<el-input></el-input>
+                      体系名称定义：
                     </td>
+                    <td><input/></td>
                   </tr>
                   <tr>
+                    <td>  测评序号选择：</td>
                     <td>
-                      测评序号选择：<el-select>
+                    <el-select>
                         <el-option label="2021" value="1"></el-option>
                         <el-option label="2022" value="2"></el-option>
                       </el-select>
                     </td>
                   </tr>
                   <tr>
+                    <td>身份类型选择:</td>
                     <td>
                         <el-checkbox-group v-model="checkList">
                           <el-checkbox label="A">企业负责人(1)</el-checkbox>
@@ -46,97 +49,137 @@
                 </table>
               </div>
               <!-- 指标内容 -->
-              <div></div>
+              <div>
+                <table border="2" width="500px">
+                  <tr>
+                    <td :rowspan="zhibiaoneirong.row">指标内容</td>
+                    <td>二级指标选择：</td>
+                    <td>
+                      <el-select>
+                        <el-option label="2021" value="1"></el-option>
+                        <el-option label="2022" value="2"></el-option>
+                      </el-select>
+                    </td>
+                    <td>权重(可在二级指标修改)</td>
+                    <td>对应一级指标</td>
+                  </tr>
+                  <tr v-for="(item,i) in zhibiaoneirong.list" :key="i">
+                    <td>{{item.a}}</td>
+                    <td><input v-text="item.b"/></td>
+                    <td><input v-text="item.c"/></td>
+                    <td><input v-text="item.d"/></td>
+                  </tr>
+                </table>
+              </div>
               <!-- 参评群体内容 -->
-              <div></div>
+              <div>
+                <table border="2" width="500px">
+                  <tr>
+                    <td :rowspan="canpingqunti.row">参评群体内容</td>
+                    <td>参评群体选择：</td>
+                    <td>
+                      <el-select>
+                        <el-option label="2021" value="1"></el-option>
+                        <el-option label="2022" value="2"></el-option>
+                      </el-select>
+                    </td>
+                    <td>权重(可在参评群体修改)</td>
+                  </tr>
+                  <tr v-for="(item, i) in canpingqunti.list" :key="i">
+                    <td>{{item.a}}</td>
+                    <td><input v-text="item.b"/></td>
+                    <td><input v-text="item.c"/></td>
+                  </tr>
+                </table>
+              </div>
               <!-- 否决性指标 -->
               <div>
-                <table>
+                <table border="2" width="500px">
                   <tr>
-                    <td rowspan="10">总体评价</td>
-                    <td rowspan="10">指标内容</td>
+                    <td rowspan="11">总体评价</td>
+                    <td rowspan="11">指标内容</td>
                   </tr>
                   <tr>
                     <td>
-                      1.<el-input></el-input>
+                      &ensp;1.<input/>
                     </td>
                   </tr>
                   <tr>
                     <td>
-                      2.<el-input></el-input>
+                      &ensp;2.<input/>
                     </td>
                   </tr>
                   <tr>
                     <td>
-                      3.<el-input></el-input>
+                      &ensp;3.<input/>
                     </td>
                   </tr>
                   <tr>
                     <td>
-                      4.<el-input></el-input>
+                      &ensp;4.<input/>
                     </td>
                   </tr>
                   <tr>
                     <td>
-                      5.<el-input></el-input>
+                      &ensp;5.<input/>
                     </td>
                   </tr>
                   <tr>
                     <td>
-                      6.<el-input></el-input>
+                      &ensp;6.<input/>
                     </td>
                   </tr>
                   <tr>
                     <td>
-                      7.<el-input></el-input>
+                      &ensp;7.<input/>
                     </td>
                   </tr>
                   <tr>
                     <td>
-                      8.<el-input></el-input>
+                      &ensp;8.<input/>
                     </td>
                   </tr>
                   <tr>
                     <td>
-                      9.<el-input></el-input>
+                      &ensp;9.<input/>
                     </td>
                   </tr>
                   <tr>
                     <td>
-                      10.<el-input></el-input>
+                      10.<input/>
                     </td>
                   </tr>
                 </table>
               </div>
               <!-- 总体评价 -->
               <div>
-                <table>
+                <table border="2"  width="500px">
                   <tr>
                     <td rowspan="5">总体评价</td>
                     <td rowspan="1">评价标题</td>
                     <td>
-                      <el-input></el-input>
+                      <input placeholder="请按照下放要求输入"/>
                       <br>
                       <p>“文本框型题|选项型标题|上行右边的标题|上行左边的标题”     没有为空，但"|"一定要输入</p>
                     </td>
                   </tr>
                   <tr>
                     <td rowspan="4">选项定义：</td>
-                    <td>1.<el-input></el-input></td>
+                    <td>1.<input/></td>
                   </tr>
                   <tr>
-                    <td>2.<el-input></el-input></td>
+                    <td>2.<input/></td>
                   </tr>
                   <tr>
-                    <td>3.<el-input></el-input></td>
+                    <td>3.<input/></td>
                   </tr>
                   <tr>
-                    <td>4.<el-input></el-input></td>
+                    <td>4.<input/></td>
                   </tr>
                 </table>
               </div>
             </table>
-            <span>这是一段信息</span>
+            
             <span slot="footer" class="dialog-footer">
                 <el-button @click="cancel">取 消</el-button>
                 <el-button type="primary" @click="confirmit">确 定</el-button>
@@ -156,6 +199,38 @@ export default {
       return {
         dialogVisible: true,
         checkList : [],
+        canpingqunti : {
+          row : 3,
+          list : [
+            {
+              a : '群体a',
+              b : '所在企业班子成员及各部门主要负责人',
+              c : 50
+            },
+            {
+              a : '群体b',
+              b : '所在负责人',
+              c : 100
+            }
+          ]
+        },
+        zhibiaoneirong : {
+          row : 3,
+          list : [
+            {
+              a : '群体a',
+              b : 'aa',
+              c : 'aa',
+              d : '德'
+            },
+            {
+              a : '群体b',
+              b : 'aa',
+              c : 'aa',
+              d : '德'
+            }
+          ]
+        }
       };
     },
     watch: {
